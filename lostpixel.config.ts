5d1f5de861bd2ec53b1689ec0d8e77d5b75cfb31
David Leger <davidleger95@gmail.com>
2024-04-02 10:58:02 -0300

    fix: serve built preview

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,7 +3,7 @@ import { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'home' }],
-    baseUrl: 'http://172.17.0.1:5173',
+    baseUrl: 'http://172.17.0.1:4173',
   },
   lostPixelProjectId: 'cluifcmts01bbkg0e08pepl32',
   apiKey: process.env.LOST_PIXEL_API_KEY,
